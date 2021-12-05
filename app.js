eece018d43e0988313fbc486c5a712cafcabc091
rmfkxhxh <manboong94@gmail.com>
2021-12-06 04:22:54 +0900

    commit 1206

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
-// const upload = require("multer")
 const logger = require("morgan");
 const session = require("express-session")
 const dotenv = require("dotenv").config();
+const getConnection = require("./database.js")
+const bcrypt = require('bcrypt-nodejs');
+
+getConnection((conn) => {
+    var q1 = ""
+    conn.query(
+        q1
+    );
+    conn.release()
+})
 
 
 // const personalQueryRouter= require('./routes/board/personalQuery');
@@ -43,47 +52,128 @@ app.set('view engine', 'pug');
 app.set("views", './views');
 
 app.get('/order', (req, res) => {
-    res.render('order', {page: "join"})
+    res.render('order', { page: "join" })
 })
 
 
 
 const port = 3000
-const host = '0.0.0.0'
+const host = '127.0.0.1'
 
 app.get('/', (req, res) => {
     let products = [
-        {title:"양파 치즈 마요 드레싱" , name:"부드러운 닭가슴살 콥 샐러드 (S)" , sale:[ "10%",  "5,800원"], price:"5,220원"},
-        {title:"참깨 마요 드레싱" , name:"[I like Eat] 크랜베리 치킨 샐러드" , sale:[ "10%",  "5,800원"], price:"5,220원"},
-        {title:"참깨 드레싱" , name:"율무 단호박 샐러드(R)" , sale:[ "10%",  "5,800원"], price:"5,220원"},
-        {title:"스위트 바나나 드레싱" , name:"리코타 치즈 샐러드 (S/R)" , sale:[ "10%",  "5,800원"], price:"5,220원"}
-                   ]
-    res.render('index', {breadcrumbList: ["HOME"],products: products,  page: 'event.pug'})
+        { title: "양파 치즈 마요 드레싱", name: "부드러운 닭가슴살 콥 샐러드 (S)", sale: ["10%", "5,800원"], price: "5,220원" },
+        { title: "참깨 마요 드레싱", name: "[I like Eat] 크랜베리 치킨 샐러드", sale: ["10%", "5,800원"], price: "5,220원" },
+        { title: "참깨 드레싱", name: "율무 단호박 샐러드(R)", sale: ["10%", "5,800원"], price: "5,220원" },
+        { title: "스위트 바나나 드레싱", name: "리코타 치즈 샐러드 (S/R)", sale: ["10%", "5,800원"], price: "5,220원" }
+    ]
+    if (req.session.user != undefined) {
+        
+        res.render('index', { breadcrumbList: ["HOME"], products: products, page: 'event.pug' })
+    } else {
+        res.render('event', { breadcrumbList: ["HOME"]})
+    }
+})
+
+app.get('/signup', (req, res) => {
+    res.render('signup', { breadcrumbList: ["HOME", "회원가입"] })
+})
+app.post('/signup', (req, res) => {
+    // console.log(req.body)
+    var formdata = {
+        id: req.body.id,
+        name: req.body.name,
+        pwd: req.body.password,
+        pwdchk: req.body.passwordck,
+        addr: req.body.post + '/' + req.body.addr + '/' + req.body.detai,
+        birthdate: req.body.date,
+        gender: req.body.gender,
+        phone: req.body.phone
+    }
+    if (formdata.pwd != formdata.pwdchk) {
+        console.log("비밀번호 틀림")
+        res.redirect('signup')
+    } else {
+        bcrypt.hash(formdata.pwd, null, null, function (err, hash) {
+            // insert user data into users table
+            var qSignup = "INSERT INTO customer (Id, Pwd, Name, Addr, Birthdate, Phone, Gender) VALUES ('" + formdata.id + "', '" + hash + "', '" + formdata.name + "', '" + formdata.addr + "', '" + formdata.birthdate + "', '" + formdata.phone + "', '" + formdata.gender + "');"
+            getConnection((conn) => {
+                conn.query(
+                    q1, function (err, row, fields) {
+                        if (err) throw err;
+                        // console.log(row);
+                    }
+                );
+                conn.release()
+            })
+        });
+        res.redirect('/signup')
+    }
 })
 
 app.get('/login', (req, res) => {
-    res.render('login', {breadcrumbList: ["HOME", "로그인"]})
+    res.render('login', { breadcrumbList: ["HOME", "로그인"] })
 })
-app.get('/signup', (req, res) => {
-    res.render('signup', {breadcrumbList: ["HOME", "회원가입"]})
+
+app.post('/login', (req, res) => {
+    param = [req.body.id, req.body.pw]
+    qLogin = `SELECT * FROM customer WHERE Id='${param[0]}'`
+    getConnection((conn) => {
+        conn.query(
+            qLogin, function (err, row) {
+                if (err) throw err;
+                if (row.length > 0) { //id 가 존재한다면
+                    console.log(row);
+                    bcrypt.compare(param[1], row[0].Pwd, (error, result) => {
+                        if (result) {
+                            console.log('로그인 성공');
+                            req.session.user = row[0]
+                            req.session.save(err => {
+                                if (err) {
+                                    console.log(err);
+                                    return res.status(500).send("h1 500 error");
+                                }
+                                else {
+                                    res.status(200).send('h1 OK');
+                                }
+                            })
+
+                        } else {
+                            console.log('로그인 실패')
+                            console.log("user: ", req.session.user)
+                        }
+                    })
+                } else {
+                    console.log('ID가 존재하지 않습니다.')
+                }
+            })
+        conn.release()
+    });
+    res.redirect('/')
 })
+
+app.get('/mypage', (req, res) => {
+    res.render('event', { breadcrumbList: ["HOME", "이벤트"] })
+})
+
+
 app.get('/event', (req, res) => {
-    res.render('event', {breadcrumbList: ["HOME", "이벤트"]})
+    res.render('event', { breadcrumbList: ["HOME", "이벤트"] })
 })
 app.get('/display', (req, res) => {
-    res.render('display', {breadcrumbList: ["HOME", "기획전"]})
+    res.render('display', { breadcrumbList: ["HOME", "기획전"] })
 })
 app.get('/coupon', (req, res) => {
-    res.render('coupon', {breadcrumbList: ["HOME", '쿠폰/교환권']})
+    res.render('coupon', { breadcrumbList: ["HOME", '쿠폰/교환권'] })
 })
 app.get('/detail', (req, res) => {
     res.render('detail')
 })
 app.get('/story', (req, res) => {
-    res.render('story', {breadcrumbList: ["HOME", '스토리']})
+    res.render('story', { breadcrumbList: ["HOME", '스토리'] })
 })
 app.get('/board/myPersonalQuery', (req, res) => {
-    res.render('myPersonalQuery', {breadcrumbList: ["HOME", '고객센터', '1:1 문의하기']})
+    res.render('myPersonalQuery', { breadcrumbList: ["HOME", '고객센터', '1:1 문의하기'] })
 })
 
 
