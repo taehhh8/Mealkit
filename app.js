2a693b10311aae5ff928f86f448974dc3d48f6e1
eumcloud <livelch012@naver.com>
2021-12-12 19:56:03 +0900

    에러체크중

--- app.js
+++ app.js
@@ -2,39 +2,29 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const bcrypt = require('bcrypt-nodejs');
-const session = require("express-session")
-const getConnection = require("./model/database.js")
-const member = require("./routes/member.js")
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const boardRouter= require('./routes/board');
-
-require("dotenv").config
-// const FileStore = require('session-file-store')(session)
-// require("dotenv").config();
 // const upload = require("multer")
-// const logger = require("morgan");
+const logger = require("morgan");
+const session = require("express-session")
+const getConnection = require("./static/js/database.js")
+require("dotenv").config();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/board', boardRouter);
-// getConnection((conn) => {
-//     var q1 = ""
-//     conn.query(
-//         q1
-//     );
-//     conn.release()
-// })
+
+getConnection((conn) => {
+    var q1 = ""
+    conn.query(
+        q1
+    );
+    //res.render("페이지", {result : q1});
+    // console.log(q1);
+    conn.release()
+})
+
+
+
 
 // const personalQueryRouter= require('./routes/board/personalQuery');
+
 //routes
 // const membership = require("./routes/membership");
 // const common = require("./routes/common");
@@ -42,11 +32,14 @@ app.use('/board', boardRouter);
 // app.use("/common", common);
 // const db = require("./model");
 
+
+
 app.use(express.static('./views'))
 app.use(express.static('./static'))
 
 
 // app.use('/board/personalQuery', personalQueryRouter);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -59,7 +52,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: (1000 * 60 * 100) //miliseconds
+        maxAge: (1000 * 60 * 100 ) //miliseconds
     }
 }));
 
@@ -67,37 +60,57 @@ var Users = [];
 app.set('view engine', 'pug');
 app.set("views", './views');
 
+const port = process.env.appPort;
+
+const host = process.env.appHost;
+
+app.get('/', (req, res) => {
+    getConnection((conn)=>{
+        let query = "select * from Product";
+        conn.query( query, (err, result)=>{
+            
+        
+        conn.release()
+        if (req.session.valid) {
+            res.render('index', { 
+                breadcrumbList: ["HOME"], 
+                products: products, 
+                page: 'event.pug', 
+                sessionValid: req.session.valid, 
+                user:req.session.user.id,
+                result: result
+            })
+            console.log("user: ", req.session.user)
+        } else {
+            res.render('event', { breadcrumbList: ["HOME", '비회원접근']})
+
+        }
+    });
+    })
+})
+
+
 app.get('/board/order', (req, res) => {
     res.render('order', { page: "join" })
 
 })
 
-const port = process.env.appPort
-const host = process.env.appHost
 
-app.get('/', (req, res) => {
-    // console.log(req.session.valid)
-    let products = [
-        { title: "양파 치즈 마요 드레싱", name: "부드러운 닭가슴살 콥 샐러드 (S)", sale: ["10%", "5,800원"], price: "5,220원" },
-        { title: "참깨 마요 드레싱", name: "[I like Eat] 크랜베리 치킨 샐러드", sale: ["10%", "5,800원"], price: "5,220원" },
-        { title: "참깨 드레싱", name: "율무 단호박 샐러드(R)", sale: ["10%", "5,800원"], price: "5,220원" },
-        { title: "스위트 바나나 드레싱", name: "리코타 치즈 샐러드 (S/R)", sale: ["10%", "5,800원"], price: "5,220원" }
-    ]
-    if (req.session.valid) {
-        res.render('index', {breadcrumbList: ["HOME"], products: products, page: 'event.pug', sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('event', { breadcrumbList: ["HOME", '비회원접근'] })
-    }
+app.get("/plist", (req, res)=>{
+    getConnection((conn)=>{
+    let query = "select * from Product";
+        conn.query( query, (err, result)=>{
+        conn.release()
+            res.render("index", {
+                breadcrumbList: ["HOME"],
+                result: result})
+        })
+    })
 })
-
 app.get('/signup', (req, res) => {
-    res.render('signup', { breadcrumbList: ["HOME", "회원가입"] })
+    res.render('signup', {breadcrumbList: ["HOME", "회원가입"] })
 })
 
-app.get('/signup', (req, res) => {
-    res.render('signup', { breadcrumbList: ["HOME", "회원가입"] })
-})
 
 app.post('/signup', (req, res, registchk) => {
     // console.log(req.body)
@@ -135,6 +148,8 @@ app.post('/signup', (req, res, registchk) => {
                     console.log("비밀번호 틀림")
                     res.render('signup', {
                         registchk: 3,
+                        id: formdata.id,post:formdata.addr.split("/")[0] , addr : formdata.addr.split("/")[1] , detail: formdata.addr.split("/")[2] ,birth: formdata.birthdate, gender: formdata.gender,
+                        phone: formdata.phone, name: formdata.name,
                         breadcrumbList: ["HOME", "회원가입"]
                     });
                 } else {
@@ -168,150 +183,94 @@ app.post('/signup', (req, res, registchk) => {
     }
 })
 
+
+
 app.get('/login', (req, res) => {
-    res.render('login', { breadcrumbList: ["HOME", "로그인"], signIn: 10 })
+    res.render('login', { breadcrumbList: ["HOME", "로그인"] })
 })
 
 app.post('/login', (req, res) => {
-    param = [req.body.id, req.body.pwd]
+    param = [req.body.id, req.body.pwd] //let args = {id : req.body.id, pw: req.body.pwd }
     qLogin = `SELECT * FROM Customer WHERE Id='${param[0]}'`
     getConnection((conn) => {
-        conn.query(qLogin, function (err, row) {
-            if (err) throw err;
-            if (row.length > 0) { //id 가 존재한다면
-                console.log(row);
-                bcrypt.compare(param[1], row[0].Pwd, (error, result) => {
-                    if (result) {
-                        console.log('로그인 성공');
-                        req.session.user = row[0]
-                        req.session.save(err => {
-                            if (err) {
-                                console.log(err);
-                                return res.status(500).send("h1 500 error");
-                            }
-                            else {
-                                req.session.valid = true
-                                console.log(req.session)
-                                res.redirect('/')
-                            }
-                        })
-                    } else {
-                        console.log('로그인 실패 비밀번호 틀림')
-                        console.log("user: ", req.session.user)
-                        // res.send({signIn:0})
-                        res.render('login', {
-                            breadcrumbList: ["HOME", "로그인"],
-                            signIn: 0
-                        })
-                    }
-                })
-            } else {
-                console.log('ID가 존재하지 않습니다.')
-                // res.send({signIn:2})
-                res.render('login', {
-                    breadcrumbList: ["HOME", "로그인"],
-                    signIn: 2
-                })
-            }
-        })
+        conn.query(
+            qLogin, function (err, row) {
+                if (err) throw err;
+                if (row.length > 0) { //id 가 존재한다면
+                    console.log(row);
+                    bcrypt.compare(param[1], row[0].Pwd, (error, result) => {
+                        if (result) {
+                            console.log('로그인 성공');
+                            req.session.user = row[0]
+                            req.session.save(err => {
+                                if (err) {
+                                    console.log(err);
+                                    return res.status(500).send("h1 500 error");
+                                }
+                                else {
+									req.session.valid = true
+                                    res.redirect('/')
+                                }
+                            })
+                        } else {
+                            console.log('로그인 실패 비밀번호 틀림')
+                            console.log("user: ", req.session.user)
+							// res.send({signIn:0})
+                            res.render('login', {
+                                breadcrumbList: ["HOME", "로그인"],
+                                signIn : 0
+                            })
+                        }
+                    })
+                } else {
+                    console.log('ID가 존재하지 않습니다.')
+					// res.send({signIn:2})
+                    res.render('login', {
+                        breadcrumbList: ["HOME", "로그인"],
+                        signIn : 2
+                    })
+                }
+            })
         conn.release()
-    });
+    })
     // res.redirect('/')
 })
 
-app.get('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return console.log(err);
-        }
-        res.redirect('/login');
-    });
-});
 
-
+app.get('/mypage', (req, res) => {
+    res.render('event', { breadcrumbList: ["HOME", "이벤트"] })
+})
 app.get('/event', (req, res) => {
-    if (req.session.valid) {
-        res.render('event', {breadcrumbList: ["HOME", "이벤트"], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('event', { breadcrumbList: ["HOME", "이벤트"] })
-    }
+    res.render('event', { breadcrumbList: ["HOME", "이벤트"] })
 })
 app.get('/display', (req, res) => {
-    if (req.session.valid) {
-        res.render('display', {breadcrumbList: ["HOME", "기획전"], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('display', {breadcrumbList: ["HOME", "기획전"] })
-    }
+    res.render('display', { breadcrumbList: ["HOME", "기획전"] })
 })
 app.get('/coupon', (req, res) => {
-    if (req.session.valid) {
-        res.render('coupon', {breadcrumbList: ["HOME", '쿠폰/교환권'], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('coupon', {breadcrumbList: ["HOME", '쿠폰/교환권'], sessionValid: req.session.valid})
-    }
+    res.render('coupon', { breadcrumbList: ["HOME", '쿠폰/교환권'] })
+
 })
 app.get('/detail', (req, res) => {
-    if (req.session.valid) {
-        res.render('detail', {breadcrumbList: ["HOME", '상품상세'], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('detail', {breadcrumbList: ["HOME", '상품상세'], sessionValid: req.session.valid})
-    }
+    res.render('detail')
 })
 app.get('/story', (req, res) => {
-    if (req.session.valid) {
-        res.render('story', {breadcrumbList: ["HOME", '스토리'], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('story', {breadcrumbList: ["HOME", '스토리'], sessionValid: req.session.valid})
-    }
-})
-app.get('/notice', (req, res) => {
-    if (req.session.valid) {
-        res.render('noitce', {breadcrumbList: ["HOME", '고객센터', '공지사항'], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('notice', {breadcrumbList: ["HOME", '고객센터', '공지사항'], sessionValid: req.session.valid})
-    }
-})
-app.get('/faq', (req, res) => {
-    if (req.session.valid) {
-        res.render('faq', {breadcrumbList: ["HOME", '고객센터', '자주 묻는 질문'], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('faq', {breadcrumbList: ["HOME", '고객센터', '자주 묻는 질문'], sessionValid: req.session.valid})
-    }
+    res.render('story', { breadcrumbList: ["HOME", '스토리'] })
 })
 app.get('/post', (req, res) => {
-    if (req.session.valid) {
-        res.render('post', {breadcrumbList: ["HOME", '고객센터', '1:1 문의하기'], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('post', {breadcrumbList: ["HOME", '고객센터', '1:1 문의하기'], sessionValid: req.session.valid})
-    }
+    res.render('post', { breadcrumbList: ["HOME", '고객센터', '1:1 문의하기'] })
+})
+app.get('/notice', (req, res) => {
+    res.render('notice', { breadcrumbList: ["HOME", '고객센터', '공지사항'] })
+})
+app.get('/faq', (req, res) => {
+    res.render('faq', { breadcrumbList: ["HOME", '고객센터', '자주 묻는 질문'] })
 })
 app.get('/qna', (req, res) => {
-    if (req.session.valid) {
-        res.render('qna', {breadcrumbList: ["HOME", '고객센터', '고객의 소리'], sessionValid: req.session.valid, user: req.session.user.Id})
-        // console.log("user: ", req.session.user.Id)
-    } else {
-        res.render('qna', {breadcrumbList: ["HOME", '고객센터', '고객의 소리'], sessionValid: req.session.valid})
-    }
-})
-app.get('/board', (req, res) => {
-    // if (req.session.valid) {
-    //     res.render('view', {breadcrumbList: ["HOME", '고객센터', '1:1문의하기'], sessionValid: req.session.valid, user: req.session.user.Id})
-    //     // console.log("user: ", req.session.user.Id)
-    // } else {
-        res.render('view', {breadcrumbList: ["HOME", '고객센터', '1:1문의하기'], sessionValid: req.session.valid})
-    // }
+    res.render('qna', { breadcrumbList: ["HOME", '고객센터', '고객의 소리'] })
 })
 
 
-app.use('/member', member)
+
 
 app.listen(port, host, () => {
     console.log(`application running at http://${host}:${port}/`)
